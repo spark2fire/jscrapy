c7ad944444a63b7874dd6e5c3febefb8ede45210
Leon <88978827@qq.com>
2021-03-06 22:12:19 +0800

    update dependency

--- jscrapy-application/src/main/java/cn/spark2fire/jscrapy/entity/ProcessorBean.java
+++ jscrapy-application/src/main/java/cn/spark2fire/jscrapy/entity/ProcessorBean.java
@@ -2,11 +2,13 @@ package cn.spark2fire.jscrapy.entity;
 
 import cn.spark2fire.jscrapy.processor.PageProcessor;
 
-public class ProcessorBean {
+public class ProcessorBean implements Comparable {
+    private int order;
     private String url;
     private PageProcessor processor;
 
-    public ProcessorBean(String url, PageProcessor processor) {
+    public ProcessorBean(int order, String url, PageProcessor processor) {
+        this.order = order;
         this.url = url;
         this.processor = processor;
     }
@@ -25,5 +27,19 @@ public class ProcessorBean {
 
     public void setProcessor(PageProcessor processor) {
         this.processor = processor;
+    }
+
+    @Override
+    public int compareTo(Object obj) {
+        if (obj instanceof ProcessorBean) {
+            if (this.order > ((ProcessorBean) obj).order) {
+                return 1;
+            } else if (this.order == ((ProcessorBean) obj).order) {
+                return 0;
+            } else {
+                return -1;
+            }
+        }
+        return -99;
     }
 }
---
+++ jscrapy-application/src/main/java/cn/spark2fire/jscrapy/util/PriorityQueue.java
@@ -0,0 +1,54 @@
+package cn.spark2fire.jscrapy.util;
+
+public class PriorityQueue<T extends Comparable<? super T>> {
+    private T[] array;
+    private int size = 10;
+    private int lastIdx;
+
+    public PriorityQueue() {
+        array = (T[]) new Object[size];
+        lastIdx = -1;
+    }
+
+    public void insert(T item) {
+        if (lastIdx + 1 >= size) {
+            extendSize();
+        }
+        array[++lastIdx] = item;
+        if (lastIdx > 0) {
+            siftUp(lastIdx);
+        }
+    }
+
+    private void siftUp(int idx) {
+        while (getParent(idx) >= 0) {
+            if (array[idx].compareTo(array[getParent(idx)]) < 0) {
+                T temp = array[idx];
+                array[idx] = array[getParent(idx)];
+                array[getParent(idx)] = temp;
+                idx = getParent(idx);
+            } else {
+                break;
+            }
+        }
+    }
+
+    private void siftDown() {
+    }
+
+    private int getParent(int idx) {
+        if (idx == 0) {
+            return -1;
+        } else {
+            return (idx - 1) / 2;
+        }
+    }
+
+    private void extendSize() {
+        T[] temp = (T[]) new Object[size + size / 2];
+        for (int i = 0; i < size; i++) {
+            temp[i] = array[i];
+        }
+        array = temp;
+    }
+}
